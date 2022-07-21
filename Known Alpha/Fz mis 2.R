165c27237742a8e3534bd038097c2055627e61b5
Cuong <cuongtuanpham@gmail.com>
2022-07-21 18:47:55 -0400

    add comments

--- Known Alpha/Fz mis 2.R
+++ Known Alpha/Fz mis 2.R
@@ -1,5 +1,14 @@
 ###########################################################################################
-########### This is the case where we misspecified f(Z|X) ###################
+########### This is the case where we misspecified f(Z|X) #################################
+###########################################################################################
+
+
+#Inputs: seed.l controls the randomness of the covariate L
+#        seed.y controls the randomness of the outcome Y
+#        alpha_n1 is the sensitivity parameter for alpha^- 
+#        alpha_p1 is the sensitivity parameter for alpha^+ 
+#        alpha0_n1 and alpha0_p1 is always set at 0
+#        size controls the sample size of the simulation
 
 bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 0, alpha0_p1 = 0, size){
   tryCatch({
@@ -9,7 +18,14 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
     require(matrixStats)
     require(hal9001)
     require(nnet)
-    #########################train data set################
+   
+     ###################################################################################
+    ######################### Training Model ##########################################
+    ###################################################################################
+    
+    #Part I: create data set  
+    #Create train data set
+    
     dat1 = gen.compl.data(seed.l = seed.l, seed.y = seed.y, alpha_n1 = alpha_n1, 
                           alpha_p1 = alpha_p1,  sample.size = size)
     dat = dat1
@@ -21,8 +37,12 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
     dat$wd = ifelse(dat$Z == -1, w(dat$outcome, alpha0_n1, alpha_n1), w(dat$outcome, alpha0_p1, alpha_p1))
     
     
-    ############calculate propensity score##########
+    #Part II: Estimate nuisance parameters
+    #f(A|Z,X) parameter is not correctly specified.
+    
+    ## calculate the propensity score f(Z|X)
     #propensity score is incorrectly specified
+    
     mod.logit  = glm(as.factor(Z) ~ 1, data = dat, family = binomial(link = logit))
     fz = predict(mod.logit, type = "response")
     fz = ifelse(dat$Z == 1, fz, 1 - fz)
@@ -30,14 +50,15 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
     
     
     
-    #specify correctly p(A|Z,X)
+    #specify correctly f(A|Z,X)
     mod.multi = multinom(A ~ Z + L1 + L2, data = dat)
     faz = predict(mod.multi, type = "prob")
     dat$faz = ifelse(dat$Z == 1,faz[,3],faz[,1])
     
-    ############### Doubly Robust Estimator##############################
-    dat.PI = dat #subset the train data to the data set where A = Z. Those are people with potential to be 
-    #become compliers
+    #Part III: Fitting model
+    
+    #Implement Doubly Robust Estimator (the MR method)
+    dat.PI = dat #make a copy of dat
     
     #calculate E[yw+ | A = 1, Z = 1, L1, L2]
     #Q+ and gamma+ are correctly specified
@@ -82,67 +103,68 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
     dat.PI$w = dat.PI$Z*(term.p - term.n) #the weight for SVM is Z*delta
     dat.PI$lab = sign(dat.PI$w)*dat.PI$Z
     
+    #fitting the multiply robust model 
     mod.mr = wsvm(as.factor(lab) ~ L1 + L2, data = dat.PI, case.weights = abs(dat.PI$w), kernel = "linear", 
                   cross = 10, scale =  F)
     
-    #############################################################
-    #############################################################
-    #tchetgen and proportion and OWL method
     
-    dat$w = (dat$A*(dat$Z+dat$A)*dat$outcome*dat$wd)/(2*dat$gamma*dat$fz*dat$faz+2*(dat$gamma==0)) # proposed weight
-    dat$w2 = (dat$Z*dat$A*dat$outcome)/dat$fz #Tchetgen weight
+    #Implement the IPW, IVT, and OWL method #
+    dat$w = (dat$A*(dat$Z+dat$A)*dat$outcome*dat$wd)/(2*dat$gamma*dat$fz*dat$faz+2*(dat$gamma==0)) # IPW weight
+    dat$w2 = (dat$Z*dat$A*dat$outcome)/dat$fz #IVT weight
     dat$w3 = dat$outcome/dat$fz #OWL weight
     
     dat$lab = sign(dat$w)*dat$Z
     dat$lab2 = sign(dat$w2)*dat$Z
     dat$lab3 = sign(dat$w3)*dat$Z
     
-    
     #models building
     mod.prop = wsvm(as.factor(lab) ~ L1 + L2, data = dat[dat$A==dat$Z,], case.weights = abs(dat$w[dat$A==dat$Z]), kernel = "linear", 
-                    cross = 10, scale =  F)
+                    cross = 10, scale =  F) #fitting IPW method
     
     mod.tchetgen = wsvm(as.factor(lab2) ~ L1 + L2, data = dat[dat$A!=0,], case.weights = abs(dat$w2[dat$A!=0]), kernel = "linear", 
-                        cross = 10, scale =  F)
+                        cross = 10, scale =  F) #fitting IVT method
     
     mod.owl = wsvm(as.factor(lab3) ~ L1 + L2, data = dat, case.weights = abs(dat$w3), kernel = "linear", 
-                   cross = 10, scale =  F)
+                   cross = 10, scale =  F) #fitting OWL method
     
-    ###########################################################################################################
-    ############################################## Testing ####################################################
-    ###########################################################################################################
+    #####################################################################################
+    ########################### Models Evaluation #######################################
+    #####################################################################################
+    
+    
+    # Part I: Obtained the test data set 
+    #note that we set the seed.l differs from the one that we use to generate the train set. This will make sure that the model is not 
+    #over fitting.
     
     dat1.test = gen.compl.data(seed.l = seed.l*2, seed.y = seed.y, alpha_n1 = alpha_n1, alpha_p1 = alpha_p1, sample.size = size)
     dat2.test = gen.switch.IV(seed.y = seed.y*3, alpha_n1 = alpha_n1, alpha_p1 = alpha_p1, dat2 = dat1.test)
     dat.test =  left_join(dat1.test, dat2.test, by = c("L1", "L2", "PI"), suffix = c("",".2"))
     
-    ###### Obtain the predicted treatment assignment when applying #################   
-    
-    #the proposed method (IPW)
+    #we get the predicted values for the proposed method (IPW)
     fitted.prop = predict.wsvm(mod.prop, newdata = dat.test[,c("L1", "L2")])
     fitted.prop = as.numeric(as.character(fitted.prop))
     
-    #the OWL method
+    #we get the predicted values for the OWL method
     fitted.owl = predict.wsvm(mod.owl, newdata = dat.test[,c("L1","L2")])
     fitted.owl = as.numeric(as.character(fitted.owl))
     
-    #the method proposed by Cui and Tchetgen
+    #we get the predicted values for IVT method
     fitted.tchetgen = predict.wsvm(mod.tchetgen, newdata = dat.test[,c("L1", "L2")])
     fitted.tchetgen = as.numeric(as.character(fitted.tchetgen))
     
-    #the multiply robust estimator
+    #we obtain the predicted values using the multiply robust (MR) method
     fitted.mr = predict.wsvm(mod.mr, newdata = dat.test[,c("L1","L2")])
     fitted.mr = as.numeric(as.character(fitted.mr))
     
-    ############## Calculate the value function using the monte carlo method #######################################
-    #Calculate value function for the regime D: V[D(L)] = E[ I{D(L) = 1}*E[Y(1)|L, U] + I{D(L) = -1}*E[Y(-1)|L, U]]
+    ###############
+    # Part III: Calculate the empirical value function for the regime D: V[D(L)] = E[ I{D(L) = 1}*E[Y(1)|L, U] + I{D(L) = -1}*E[Y(-1)|L, U] ]
     
-    #Cui and Tchetgen method
+    #the value function using IVT method
     value.tchetgen = ifelse(fitted.tchetgen[dat.test$PI == 4] == dat.test$Z[dat.test$PI == 4], dat.test$outcomec[dat.test$PI == 4]
                             , dat.test$outcomec.2[dat.test$PI == 4])
     value.tchetgen.mc = mean(value.tchetgen,na.rm = T)
     
-    #the IPW method 
+    #IPW method (proposed method)
     value.binary = ifelse(fitted.prop[dat.test$PI == 4] == dat.test$Z[dat.test$PI == 4], dat.test$outcomec[dat.test$PI == 4]
                           , dat.test$outcomec.2[dat.test$PI == 4])
     value.prop.mc = mean(value.binary,na.rm = T)
@@ -152,42 +174,41 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
                             , dat.test$outcomec.2[dat.test$PI == 4])
     value.owl.mc = mean(value.func.owl,na.rm = T)
     
-    #the multiply robust estimator
+    #the multiply robust (MR) estimator
     value.mr = ifelse(fitted.mr[dat.test$PI == 4] == dat.test$Z[dat.test$PI == 4], dat.test$outcomec[dat.test$PI == 4]
                       , dat.test$outcomec.2[dat.test$PI == 4])
     value.mr.mc = mean(value.mr, na.rm = T)
     
     
     
-    ###### Calculate the correct classification rate ##################
+    # Part IV: Estimate the correct classification rate
     
-    #true classification
+    #determine the true correct rate
     opt.regime = ifelse(dat.test$outcomec > dat.test$outcomec.2, dat.test$Z, dat.test$Z.2)
     opt.regime = factor(opt.regime, levels = c(-1,1))
     
-    #correct classification rate for the IPW method
+    #determine the true correct rate for IPW method
     tab1 = table(opt.regime[dat.test$PI == 4] , factor(fitted.prop[dat.test$PI == 4], levels = c(-1,1))) 
     correct.rate1 = (tab1[1,1] + tab1[2,2])/sum(tab1)
     
-    #correct classification rate for the OWL method
+    #determine the true correct rate for OWL method
     tab.owl = table( opt.regime[dat.test$PI == 4], factor(fitted.owl[dat.test$PI == 4], levels = c(-1,1)))
     correct.rate.owl = (tab.owl[1,1] + tab.owl[2,2])/sum(tab.owl)
     
-    #correct classification rate for the Cui and Tchetgen method
+    #determine the true correct rate for IVT method
     tab2 = table(opt.regime[dat.test$PI == 4] , factor(fitted.tchetgen[dat.test$PI == 4], levels = c(-1,1)))
     correct.rate.tchetgen = (tab2[1,1] + tab2[2,2])/sum(tab2)
     
-    #correct classfication rate for the multiply robust method
+    #determine the true correct rate for MR method
     tab3 = table(opt.regime[dat.test$PI == 4] , factor(fitted.mr[dat.test$PI == 4], levels = c(-1,1))) 
     correct.rate3 = (tab3[1,1] + tab3[2,2])/sum(tab3)
     
     ######### The maximal value function ###################### 
     true.value.func = mean(pmax(dat.test$outcomec, dat.test$outcomec.2),na.rm = T)
     
+    # Part V: Estimate the value functions
     
-    #######Calculate value function using the multiply robust method #######################
-    ########################################################################################
-    
+    #calculate the value function using the multiple robust method
     test.df = dat.test
     
     
@@ -197,12 +218,12 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
     fz3 = ifelse(test.df$Z == 1, fz3, 1 - fz3)
     test.df$fz = fz3 #propensity score
     
-    #Correctly specified p(A|Z,X)
+    #Correctly specified f(A|Z,X)
     mod.multi2 = multinom(A ~ Z + L1 + L2, data = test.df)
     faz2 = predict(mod.multi2, type = "prob")
     test.df$faz = ifelse(test.df$Z == 1,faz2[,3],faz2[,1])
     
-    
+    #calculate the weight w+(Y), w-(Y) and the indicator I{pi(X) = Z}
     test.df$wd = ifelse(test.df$Z == -1, w(test.df$outcome, alpha0_n1, alpha_n1), w(test.df$outcome, alpha0_p1, alpha_p1))
     test.df$wd2 = test.df$wd
     test.df$wd2[test.df$A!= test.df$Z] = 0
@@ -275,12 +296,18 @@ bi.sim.fz.mis = function(seed.l, seed.y, alpha_n1 = 0, alpha_p1 = 0,alpha0_n1 = 
       (test.df$Z == -1)*(fitted.prop == test.df$Z)*test.df$deltan/test.df$fz
     
     
+    #value function
     
+    # MR estimator of value function
     value.function.mr = mean(term1 - (term2.1 - term2.2) - (term3.1 + term3.2) - (term4.1 - term4.2))
     
-    ############### Calculate the value function using the regular non-robust method ###############################
+    ## IPW estimator of value function
     value.function.est = mean(term1)
+    
+    #calculate the value function for the method proposed in Eric's using regular value function formula (non-robust)
     value.tchetgen.th = mean((test.df$Ie*test.df$outcome*test.df$wd2)/(test.df$p.gamma*test.df$fz*test.df$faz+2*(test.df$p.gamma==0) ))
+    
+    #calculate the value function for the method proposed in Eric's using the robust value function formula
     value.mr.th =  mean((test.df$Ir*test.df$outcome*test.df$wd2)/(test.df$p.gamma*test.df$fz*test.df$faz+2*(test.df$p.gamma==0) ))
     ######################################
     
